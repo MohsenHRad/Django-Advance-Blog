fb732243eaa3b050df95d3b8b2ae38f73d4d95b7
Mohsen <mohsen.h.rad.82@gmail.com>
2025-09-01 13:02:34 +0330

    Versioning the project and make blog/api/vi/urls to do that and even change the views

--- core/blog/urls.py
+++ core/blog/urls.py
@@ -1,12 +1,12 @@
-from django.urls import path
+from django.urls import path, include
 from django.views.generic.base import RedirectView
 
 from . import views
 
-app_name = 'blog'
+app_name = "blog"
 urlpatterns = [
-    path('django/<int:pk>', views.RedirectToDjango.as_view(), name='redirect-to-django'),
-    # path('cbv-index', TemplateView.as_view(template_name='index.html',extra_context={'name':'mohsen '}))
+    # path('django/<int:pk>', views.RedirectToDjango.as_view(), name='redirect-to-django'),
+    # # path('cbv-index', TemplateView.as_view(template_name='index.html',extra_context={'name':'mohsen '}))
     path('cbv-index', views.IndexView.as_view(), name='cbv-index'),
     path(
         'go-to-index',
@@ -17,6 +17,7 @@ urlpatterns = [
     path('post/<int:pk>/', views.PostDetailView.as_view(), name='post-detail'),
     path('post/create/', views.PostCreateView.as_view(), name='post-create'),
     path('post/<int:pk>/edit/', views.PostEditView.as_view(), name='post-edit'),
-    path('post/<int:pk>/delete/', views.PostDeleteView.as_view(), name='post-delete')
-
+    path('post/<int:pk>/delete/', views.PostDeleteView.as_view(), name='post-delete'),
+    # path("post", views.api_post_list_view,name="api_post_list")
+    path('api/v1/', include('blog.api.v1.urls'))
 ]
