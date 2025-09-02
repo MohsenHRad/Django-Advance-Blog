64523ace820070a3300f6033f87fb6e096b08727
Mohsen <mohsen.h.rad.82@gmail.com>
2025-09-02 20:32:41 +0330

    - change post detail view : add PUT and DELETE method

--- core/blog/api/v1/views.py
+++ core/blog/api/v1/views.py
@@ -1,12 +1,13 @@
 from django.shortcuts import get_object_or_404
+from rest_framework import status
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
-from .serializers import PostSerializer, PostModelSerializer
+from .serializers import PostModelSerializer
 from ...models import Post
 
 
-@api_view(['GET','POST'])
+@api_view(['GET', 'POST'])
 def postList(request):
     if request.method == 'GET':
         posts = Post.objects.filter(status=True)
@@ -19,11 +20,20 @@ def postList(request):
         return Response(serializer.data)
 
 
-@api_view()
+@api_view(['GET', 'PUT', 'DELETE'])
 def postDetail(request, id):
     post = get_object_or_404(Post, pk=id, status=True)
-    serializer = PostSerializer(post)
-    return Response(serializer.data)
+    if request.method == 'GET':
+        serializer = PostModelSerializer(post)
+        return Response(serializer.data)
+    elif request.method == 'PUT':
+        serializer = PostModelSerializer(post, data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data)
+    elif request.method == 'DELETE':
+        post.delete()
+        return Response({'detail': 'Item has been deleted successfully'}, status=status.HTTP_204_NO_CONTENT)
 
     # try:
     #     post = Post.objects.get(pk=id)
