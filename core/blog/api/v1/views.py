c4d8a1cd01e2b5d4f4d38e0c998163e7eb432559
Mohsen <mohsen.h.rad.82@gmail.com>
2025-09-05 13:38:32 +0330

    feat: changing API view structure
    change API views from FBV to CBV
    
    define class base view and get and post views

--- core/blog/api/v1/views.py
+++ core/blog/api/v1/views.py
@@ -1,25 +1,45 @@
 from django.shortcuts import get_object_or_404
 from rest_framework import status
 from rest_framework.decorators import api_view, permission_classes
-from rest_framework.permissions import IsAuthenticated, IsAuthenticatedOrReadOnly
+from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
 from rest_framework.response import Response
+from rest_framework.views import APIView
 
 from .serializers import PostModelSerializer
 from ...models import Post
 
 
-@api_view(['GET', 'POST'])
-@permission_classes([IsAuthenticatedOrReadOnly])
-def postList(request):
-    if request.method == 'GET':
+class PostList(APIView):
+    """
+    Retrieving a list of Posts and Creating a new post
+    """
+    # permission_classes = [IsAuthenticated]
+    def get(self, request):
+        """ Retrieving a list of Posts """
         posts = Post.objects.filter(status=True)
         serializer = PostModelSerializer(posts, many=True)
-        return Response(serializer.data)
-    elif request.method == 'POST':
+        return Response(data=serializer.data, status=status.HTTP_200_OK)
+
+    def post(self, request):
+        """ Creating a new post with provided data. """
         serializer = PostModelSerializer(data=request.data)
         serializer.is_valid(raise_exception=True)
         serializer.save()
-        return Response(serializer.data)
+        return Response(data=serializer.data, status=status.HTTP_201_CREATED)
+
+
+# @api_view(['GET', 'POST'])
+# @permission_classes([IsAuthenticated])
+# def postList(request):
+#     if request.method == 'GET':
+#         posts = Post.objects.filter(status=True)
+#         serializer = PostModelSerializer(posts, many=True)
+#         return Response(serializer.data)
+#     elif request.method == 'POST':
+#         serializer = PostModelSerializer(data=request.data)
+#         serializer.is_valid(raise_exception=True)
+#         serializer.save()
+#         return Response(serializer.data)
 
 
 @api_view(['GET', 'PUT', 'DELETE'])
