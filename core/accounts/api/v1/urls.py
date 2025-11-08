85cfd9fab081bc5277ead6e8e2a7d84417d39f46
Mohsen <mohsen.h.rad.82@gmail.com>
2025-11-08 19:15:15 +0330

    Define url for login token generator

--- core/accounts/api/v1/urls.py
+++ core/accounts/api/v1/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
+# from rest_framework.authtoken.views import ObtainAuthToken
 
 from . import views
 
 urlpatterns = [
-    path('registration/', views.RegistrationApiView.as_view(), name='registration')
+    path('registration/', views.RegistrationApiView.as_view(), name='registration'),
+    path('token/login/', views.CustomObtainAuthToken.as_view(), name='token-login')
 ]
