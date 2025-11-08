5f5c370267dd84ec85afe43155544a8773c4ce39
Mohsen <mohsen.h.rad.82@gmail.com>
2025-11-08 20:56:42 +0330

    add logout url to delete token and log user out

--- core/accounts/api/v1/urls.py
+++ core/accounts/api/v1/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
-# from rest_framework.authtoken.views import ObtainAuthToken
 
 from . import views
 
+# from rest_framework.authtoken.views import ObtainAuthToken
+
 urlpatterns = [
     path('registration/', views.RegistrationApiView.as_view(), name='registration'),
-    path('token/login/', views.CustomObtainAuthToken.as_view(), name='token-login')
+    path('token/login/', views.CustomObtainAuthToken.as_view(), name='token-login'),
+    path('token/logout/', views.CustomDiscardAuthToken.as_view(), name='token-logout')
 ]
