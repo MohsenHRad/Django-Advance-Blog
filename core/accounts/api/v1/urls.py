4ce0481c9c51c72e402396a601487b0264647952
Mohsen <mohsen.h.rad.82@gmail.com>
2025-11-10 18:07:53 +0330

    add url for jwt authentication

--- core/accounts/api/v1/urls.py
+++ core/accounts/api/v1/urls.py
@@ -1,11 +1,22 @@
 from django.urls import path
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+    TokenVerifyView
+)
 
 from . import views
 
 # from rest_framework.authtoken.views import ObtainAuthToken
 
+app_name = 'api-v1'
+
 urlpatterns = [
     path('registration/', views.RegistrationApiView.as_view(), name='registration'),
     path('token/login/', views.CustomObtainAuthToken.as_view(), name='token-login'),
-    path('token/logout/', views.CustomDiscardAuthToken.as_view(), name='token-logout')
+    path('token/logout/', views.CustomDiscardAuthToken.as_view(), name='token-logout'),
+    path('jwt/create/', TokenObtainPairView.as_view(), name='jwt-create'),
+    path('jwt/refresh/', TokenRefreshView.as_view(), name='jwt-refresh'),
+    path('jwt/verify/', TokenVerifyView.as_view(), name='jwt-verify')
+
 ]
