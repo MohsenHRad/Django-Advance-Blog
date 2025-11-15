9f932ddd19e66f731030ca85f70654dc209da71d
Mohsen <mohsen.h.rad.82@gmail.com>
2025-11-15 18:50:02 +0330

    add profiles urls to sprate urls

---
+++ core/accounts/api/v1/urls/profiles.py
@@ -0,0 +1,15 @@
+from django.urls import path
+from rest_framework_simplejwt.views import (
+    TokenRefreshView,
+    TokenVerifyView
+)
+
+from .. import views
+
+# from rest_framework.authtoken.views import ObtainAuthToken
+
+urlpatterns = [
+    # profile
+    path('', views.ProfileApiView.as_view(), name='profile')
+
+]
