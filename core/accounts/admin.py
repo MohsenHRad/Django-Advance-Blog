6d1422af495c4e9a4f72cdf9ff931437c1c719a1
Mohsen <mohsen.h.rad.82@gmail.com>
2025-11-15 18:47:12 +0330

    add verified field to the admin page

--- core/accounts/admin.py
+++ core/accounts/admin.py
@@ -6,8 +6,8 @@ from .models import User, Profile
 
 class CustomUserAdmin(UserAdmin):
     model = User
-    list_display = ('email', 'is_superuser', 'is_active')
-    list_filter = ('email', 'is_superuser', 'is_active')
+    list_display = ('email', 'is_superuser', 'is_active','is_verified')
+    list_filter = ('email', 'is_superuser', 'is_active','is_verified')
     search_fields = ('email',)
     ordering = ('email',)
     fieldsets = (
@@ -18,7 +18,7 @@ class CustomUserAdmin(UserAdmin):
         ),
         (
             'Permissions', {
-            'fields': ['is_active', 'is_staff', 'is_superuser']
+            'fields': ['is_active', 'is_staff', 'is_superuser', 'is_verified']
         }
         ),
         (
@@ -34,7 +34,7 @@ class CustomUserAdmin(UserAdmin):
         (
             'Authentication', {
             'fields': [
-                'email', 'password1', 'password2'
+                'email', 'password1', 'password2','is_verified'
             ]
         }
         ),
